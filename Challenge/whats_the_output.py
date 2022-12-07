ede5508cef95f105103f8f95d3474f890514bc59
Shalaby <57592040+Shalabyelectronics@users.noreply.github.com>
2022-12-08 00:18:02 +0300

    Course challenges folder

---
+++ Challenge/whats_the_output.py
@@ -0,0 +1,23 @@
+# 1. What’s the output of the following program?
+
+print(“Practice Exercise 1:”)
+
+# -- OutPut --> Practice Exercise 1:
+
+# A message for the user
+
+message = “This is going to be tricky. ;)”
+Message = “Very tricky!”
+print(Message)
+
+# -- OutPut --> Very tricky!
+ 
+ # Perform mathematical operations
+
+result = 2**3
+print(“2**3 =”, result)
+# -- OutPut --> 2**3 = 8 
+result = 5 - 3
+print(“5 - 3 =”, result)
+# -- OutPut --> 5-3 = 2
+
